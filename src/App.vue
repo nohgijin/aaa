86e1a311f90e7b89f343f205292d1eb77f795fee
nohgijin <ynda11111@naver.com>
2025-12-29 18:02:52 +0900

    feat: 임시/LNB 분리

--- src/App.vue
+++ src/App.vue
@@ -6,6 +6,9 @@
 </template>
 
 <script>
+// Module Federation을 사용하여 remote에서 컴포넌트 로드
+// 동일 프로젝트 내에서는 일반적으로 직접 import를 사용하지만,
+// 다른 프로젝트에서 이 컴포넌트를 사용할 수 있도록 remote로 노출됨
 import HelloWorld from './components/HelloWorld.vue'
 
 export default {
--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,30 @@
 const { defineConfig } = require('@vue/cli-service')
+const ModuleFederationPlugin = require('webpack').container.ModuleFederationPlugin
+
 module.exports = defineConfig({
-  transpileDependencies: true
+  transpileDependencies: true,
+  configureWebpack: {
+    plugins: [
+      new ModuleFederationPlugin({
+        name: 'my_project',
+        filename: 'remoteEntry.js',
+        exposes: {
+          './HelloWorld': './src/components/HelloWorld.vue'
+        },
+        shared: {
+          vue: {
+            singleton: true,
+            requiredVersion: '^2.7.0',
+            eager: false
+          }
+        }
+      })
+    ]
+  },
+  devServer: {
+    port: 8080,
+    headers: {
+      'Access-Control-Allow-Origin': '*'
+    }
+  }
 })
