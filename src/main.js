8d30e484e9773128dc93e554b2420fc20387fb44
nohgijin <ynda11111@naver.com>
2025-12-29 18:44:50 +0900

    fix: test

--- src/main.js
+++ src/main.js
@@ -1,8 +1,9 @@
-import Vue from 'vue'
-import App from './App.vue'
+/**
+ * Module Federation Bootstrap Entry Point
+ * 
+ * 이 파일은 Module Federation이 초기화된 후에 실제 애플리케이션을 로드합니다.
+ * Bootstrap 분리 방식을 사용하여 "eager consumption" 오류를 방지합니다.
+ */
 
-Vue.config.productionTip = false
-
-new Vue({
-  render: h => h(App),
-}).$mount('#app')
+// Module Federation이 초기화된 후 bootstrap.js를 동적으로 로드
+import('./bootstrap')
